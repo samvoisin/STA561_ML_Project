2afb1c594948feb03a4dd8938fee27d2bbf2e173
Eduardo Coronado <e.coronado@duke.edu>
2019-04-08 13:48:30 -0500

    Updated samplingrate to 1kHz plus add comments

--- clean_emg_data.R
+++ clean_emg_data.R
@@ -3,54 +3,65 @@
 library(biosignalEMG)
 suppressMessages(library(tidyverse))
 
+# Helper function to clean sEMG data
+## Input: txt file_path, out directory path, subfolder to create (e.g. 01, 02, etc)
 clean_semg = function(fil, dat_out, sub_f){
   
-  dat = read_tsv(fil, col_names = TRUE) 
-  mov  = dat %>% select(class) %>% unlist() %>% factor()
+  dat = read_tsv(fil, col_names = TRUE) # Read txt file
+  mov  = dat %>% select(class) %>% unlist() %>% factor() # extract class, turn into factor
   
-  dat = dat %>% select(-time, -class) %>% 
-    as.emg(., samplingrate = 100) # Set sampling rate at 100 ms
+  dat = dat %>% select(-time, -class) %>% # Convert to EMG data type
+    as.emg(., samplingrate = 1000) # Set sampling rate at 1kHz
   
-  ch1 = envelope(dat, method = "RMS", wsize = 200, channel = 1)$values
-  tmp = matrix(NA, length(ch1), 10)
-  tmp[, 1] = seq(2, length(ch1) + 1, by= 1)
-  tmp[, 2] = ch1
+  ch1 = envelope(dat, method = "RMS", 
+                 wsize = 200, channel = 1)$values # Apply RMS to 1st channel
+  tmp = matrix(NA, length(ch1), 10) # Initialize matrix
+  tmp[, 1] = seq(2, length(ch1) + 1, by= 1) #Set time vals in ms (i.e. 2 = 200 ms)
+  tmp[, 2] = ch1 # Add RMS channel 1 vals
   
+  # Do same RMS process to rest of channels
   for(i in 2:8){
     tmp[, i+1] = envelope(dat, method = "RMS", wsize = 200, channel = i)$values
   }
   
+  # Creat df and add class column
   df = tmp %>% as.data.frame() %>% 
     as.tibble() %>%  
     setNames(., c("time", dat$data.name, "class")) %>% 
     mutate(class = mov)
  
-  
+  # Create subfolder in out_directory if doesn't exist
   dat_out = file.path(dat_out, sub_f)
   if (!dir.exists(dat_out)){
     dir.create(dat_out)
   }
-  new_filname = basename(fil) %>% str_replace(., "raw", "clean")
-  dat_out = paste0(dat_out,"/", new_filname)
-  write_tsv(df, dat_out)
+  
+  new_filname = basename(fil) %>% 
+    str_replace(., "raw", "clean") # File name change from raw to clean
+  dat_out = paste0(dat_out,"/", new_filname) # creat file out path
+  write_tsv(df, dat_out) # write file as txt
   
 }
+# End of helper function
 
-data_path = "EMG_data_for_gestures-master" 
-full_path = file.path(getwd(), data_path)
 
-out_dir = paste0("clean_", data_path)
+# Main script
+data_path = "EMG_data_for_gestures-master" # raw data folder
+full_path = file.path(getwd(), data_path) # create full path to raw data
+
+out_dir = paste0("clean_", data_path) # Create new clean data folder if non-existent
 if (!dir.exists(file.path(getwd(), out_dir))){
   dir.create(file.path(getwd(), out_dir))
 }
 
 out_path = file.path(getwd(), out_dir)
 
-data_dirs = list.dirs(full_path)[-1]
+data_dirs = list.dirs(full_path)[-1] # Get all subfolders in raw data main folder
 
+# For each subfolder (i.e. 01, 02, 03, etc)
 for (d in data_dirs){
-  files = list.files(d, full.names = TRUE)
-  sub_f = basename(d)
-  sapply(files, function(x) clean_semg(x, out_path, sub_f))
+  files = list.files(d, full.names = TRUE) # Get 2 files
+  sub_f = basename(d) #Get subfolder name to use in helper fcn and keep folder struct
+  sapply(files, function(x) clean_semg(x, out_path, sub_f)) # Apply helper fcn to both files
 }
 
