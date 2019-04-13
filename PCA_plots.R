4a03740b48a1e847780ecbe39a412ba32f8bdc68
SebastianKnigge <sebastian-knigge@live.de>
2019-04-13 18:11:03 -0400

    PCA Plots pdf and png (not with sams stylesheet for ggplot yet)

--- PCA_plots.R
+++ PCA_plots.R
@@ -8,6 +8,8 @@
 
 #library(here)
 library(dplyr)
+library(ggplot2)
+library(gridExtra)
 
 # same reading part as in the PCA data file
 main_directory <- getwd()
@@ -38,8 +40,36 @@ M <- M[-1,]
 X <- M[,names(M)!="time"]
 # scaling and using PCA
 pca1 <- princomp(scale(X))
-# safe as data frame for splitting
-pca_df <- as.data.frame(pca1$scores)
+
+
+
+
+
+
+### Plotting
+
+
+Plot_df <- data.frame(prop_var=pca1$sdev^2/sum(pca1$sdev^2), comp=names(pca1$sdev),
+                      cumulative=cumsum(pca1$sdev^2)/sum(pca1$sdev^2))
+
+p1 <- ggplot(data=Plot_df, aes(x=comp, y=prop_var)) +
+  geom_bar(stat="identity")+
+  ylab("proportional variance")+
+  ggtitle("Screeplot")+
+  theme(plot.title = element_text(hjust = 0.5), axis.title.x=element_blank(),
+        axis.text.x = element_text(angle = 60, hjust = 1))
+
+
+
+
+p2 <- ggplot(data=Plot_df, aes(x=comp, y=cumulative, group=1)) +
+  geom_line()+
+  geom_point()+
+  ylab("cumulative proportional variance")+
+  ggtitle("Cumulative variance plot")+
+  theme(plot.title = element_text(hjust = 0.5), axis.title.x=element_blank(),
+        axis.text.x = element_text(angle = 60, hjust = 1))
+
 
 
 # Create new clean data folder if non-existent
@@ -49,12 +79,15 @@ if (!dir.exists(file.path(main_directory, out_dir))){
 }
 
 
+png(filename=file.path(main_directory, out_dir, "PCA_screeplot.png"),
+    width = 900, height = 500)
+grid.arrange(p1, p2, ncol=2)
+dev.off()
 
-
-
-
-
-
+pdf(file=file.path(main_directory, out_dir, "PCA_screeplot.pdf"),
+    width = 9, height = 5)
+grid.arrange(p1, p2, ncol=2)
+dev.off()
 
 
 
